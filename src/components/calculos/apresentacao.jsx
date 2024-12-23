f118683d5705ad71cd401a9bc2fb6de5bcc4d68c
NicolasDevJS <131040191+NicolasDevJS@users.noreply.github.com>
2024-12-23 14:00:18 -0300

    deploy 2

--- src/components/calculos/apresentacao.jsx
+++ src/components/calculos/apresentacao.jsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import styled from "styled-components";
-import data from "@/data/CalculosData";
+import data from "../../data/calculosData";
 
 const ApresentacaoContainer = styled.div`
   width: 100%;
