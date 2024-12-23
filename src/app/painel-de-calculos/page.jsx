54a2dc25dd2603f27846a1d682d209c35c05010c
NicolasDevJS <131040191+NicolasDevJS@users.noreply.github.com>
2024-12-23 01:23:49 -0300

    build

--- src/app/painel-de-calculos/page.jsx
+++ src/app/painel-de-calculos/page.jsx
@@ -1,12 +1,10 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 import Header from "../../components/common/Header";
 import FooterR from "@/components/common/FooterR";
 import Apresentacao from "@/components/calculos/apresentacao";
 import Loader from "@/components/common/Loader";
-
 
 export default function PainelDeCalculos() {
   const [loading, setLoading] = useState(true);
